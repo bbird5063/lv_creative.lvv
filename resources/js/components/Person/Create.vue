9b51e3a76ab75877f6d9baf965819a8d2d1c4e67
bbird5063 <bbird5063@gmail.com>
2024-04-22 02:51:29 +0300

    26. CRUD в SPA приложении. Форма добавления, добавление и редирект

--- resources/js/components/Person/Create.vue
+++ resources/js/components/Person/Create.vue
@@ -1,18 +1,47 @@
 <template>
-	<div>
-		Create
+	<div class="w-25">
+		<div class="mb-3">
+			<input type="text" v-model="name" placeholder="name" class="form-control">
+		</div>
+		<div class="mb-3">
+			<input type="number" v-model="age" placeholder="age" class="form-control">
+		</div>
+		<div class="mb-3">
+			<input type="text" v-model="job" placeholder="job" class="form-control">
+		</div>
+		<div class="mb-3">
+			<input @click.prevent="store" type="submit" value="Add" class="btn btn-primary">
+		</div>
 	</div>
 </template>
 
 <script>
+
+import router from '../../router'; // .js(?) в router прийдет, то что "export default router"
 export default {
 	name: 'Create',
 	data() {
 		return {
+			name: null,
+			age: null,
+			job: null,
 		}
 	},
 
 	methods: {
+		store() {
+			// 'api/people/' из routes\api.php: 'prefix' => 'people'
+			axios.post('/api/people/', { name: this.name, age: this.age, job: this.job }) // РАБОТАЕТ
+			//axios.post('/api/people/', 'name=' + this.name + '&age=' + this.age + '&job=' + this.job) // РАБОТАЕТ
+				.then(res => { // в function() не будет работать 'this.'! Только в стрелочной!
+					/* В БД все появилось. Можно очистить поля, но обычно делают редирект */
+					console.log('--router--');
+					console.log(this.name);
+					console.log(router);
+					//router.push('person.index'); // (push тоже, что redirect) НЕ РАБОТАЕТ
+					router.push('/people'); // РАБОТАЕТ
+				})
+		},
 	}
 }
 </script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -2,9 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
 	{
-		//path: '/people', component: function () { return import('./components/Person/Index.vue') },
-		path: '/people', component: () => import('./components/Person/Index.vue'), // стрелочна функция
-		name: 'person.index'
+		path: '/people', component: () => import('./components/Person/Index.vue'),
+		name: 'person.index' // "name:... " не работает в js, работает в '<router-link :to="{ name: 'person.index' }"'>, т.е. только в блоке <template> 
+	},
+	{
+		path: '/people/create', component: () => import('./components/Person/Create.vue'),
+		name: 'person.create'
 	},
 
 ];
